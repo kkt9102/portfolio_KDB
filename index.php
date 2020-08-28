f57ce9cc2daf45519c480fc32683ba96f2bb7707
kkt9102 <kkt9102@gmail.com>
2020-08-28 18:45:58 +0900

    20200828 메인페이지

--- index.php
+++ index.php
@@ -15,17 +15,17 @@
   <!-- stylesheet -->
   <link rel="shortcut icon" href="https://kkt9102.github.io/img1/blog/article/portfolio/kdb/favicon.ico"
     type="image/x-icon">
-  <link rel="stylesheet" href="css/common.css">
-  <script src="js/common.js"></script>
+  <link rel="stylesheet" href="kdb.css">
+  <script src="kdb.js"></script>
 </head>
 
 <body>
   <!-- fullpage-move-menu -->
   <ul id="menu" class="fixed">
-    <li data-menuanchor="anchor1" class="with-kdb relative"><a href="#anchor1">With KDB</a></li>
-    <li data-menuanchor="anchor2" class="news-event relative"><a href="#anchor2">News & Event</a></li>
-    <li data-menuanchor="anchor3" class="special-kdb"><a href="#anchor3">Special KDB</a></li>
-    <li data-menuanchor="anchor4" class="section-4-move"><a href="#anchor4">기업금융정보</a></li>
+    <li data-menuanchor="anchor1" class="with-kdb relative color"><a href="#anchor1">With KDB</a></li>
+    <li data-menuanchor="anchor2" class="news-event relative color"><a href="#anchor2">News & Event</a></li>
+    <li data-menuanchor="anchor3" class="special-kdb color"><a href="#anchor3">Special KDB</a></li>
+    <li data-menuanchor="anchor4" class="section-4-move color"><a href="#anchor4">기업금융정보</a></li>
   </ul>
   <!-- top-menu-bar HTML -->
   <div class="top-menu-bar fixed">
@@ -59,60 +59,126 @@
     </div>
   </div>
   <div id="fullpage">
+    <!-- section .1 HTML -->
     <div class="section">
       <div class="main-slide-banner relative">
         <ul class="image-slider">
-          <li class="banner-1 active"></li>
-          <li class="banner-2 color-fff"></li>
-          <li class="banner-3 color-fff"></li>
-          <li class="banner-4 color-fff"></li>
-          <li class="banner-5 color-fff"></li>
-        </ul>
-        <ul class="text-slider color-fff con-1 relative">
-          <li class="active">
-            <div class="title-text">
-              희망의 내일,<br>
-              언제나 고객과 함께 합니다.
-            </div>
-            <div class="sub-title">
-              <a href="#">대한민국을 미래로 연결하는 금융플랫폼<span class="icon"></span>
-              </a>
+          <li class="banner-1 active">
+            <div class="text-box con-1 color-fff">
+              <div class="title-text">
+                희망의 내일,<br>
+                언제나 고객과 함께 합니다.
+              </div>
+              <div class="sub-title">
+                <a href="#">대한민국을 미래로 연결하는 금융플랫폼<span class="icon"></span>
+                </a>
+              </div>
             </div>
           </li>
-          <li>
-            <div class="title-text">혁신성장 선도</div>
-            <div class="sub-title">기업금융 전문은행으로써
-              혁신기업의 성장을 지원하고 미래형 신산업을 육성하여
-              4차 산업혁명 중심의 산업구조 변화를 선도합니다.</div>
-          </li>
-          <li>
-            <div class="title-text">
-              중소·중견기업
-              육성 주도
-            </div>
-            <div class="sub-title">한국경제의 중추인 중소·중견기업의
-              동반성장 파트너로서 맞춤형 솔루션을 제공하고
-              '중소-중견-대기업'으로 이어지는 성장의 선순환 생태계를 조성합니다.
+          <li class="banner-2 ">
+            <div class="text-box con-1 color-fff">
+              <div class="title-text">혁신성장 선도</div>
+              <div class="sub-title">기업금융 전문은행으로써<br>
+                혁신기업의 성장을 지원하고 미래형 신산업을 육성하여<br>
+                4차 산업혁명 중심의 산업구조 변화를 선도합니다.</div>
             </div>
           </li>
-          <li>
-            <div class="title-text">글로벌 금융시장 개척</div>
-            <div class="sub-title">
-              투자개발형 해외PF금융,
-              국제기구와 공동사업 발굴 등
-              해외 新 시장을 개척하고 국내기업의 해외진출을 적극 지원합니다.
+          <li class="banner-3">
+            <div class="text-box con-1 color-fff">
+              <div class="title-text">
+                중소·중견기업<br>
+                육성 주도
+              </div>
+              <div class="sub-title">한국경제의 중추인 중소·중견기업의<br>
+                동반성장 파트너로서 맞춤형 솔루션을 제공하고<br>
+                '중소-중견-대기업'으로 이어지는 성장의 선순환 생태계를 조성합니다.
+              </div>
             </div>
           </li>
-          <li>
-            <div class="title-text">사회적 역할 제고</div>
-            <div class="sub-title">대표 정책금융기관으로서
-              사회적 금융의 체계적 추진을 통해
-              기업의 사회적 책임 이행을 유도하고 상생과 협력을 촉진합니다.
+          <li class="banner-4">
+            <div class="text-box con-1 color-fff">
+              <div class="title-text">글로벌 금융시장 개척</div>
+              <div class="sub-title">
+                투자개발형 해외PF금융,<br>
+                국제기구와 공동사업 발굴 등<br>
+                해외 新 시장을 개척하고 국내기업의 해외진출을 적극 지원합니다.
+              </div>
             </div>
           </li>
+          <li class="banner-5">
+            <div class="text-box con-1 color-fff">
+              <div class="title-text">사회적 역할 제고</div>
+              <div class="sub-title">대표 정책금융기관으로서<br>
+                사회적 금융의 체계적 추진을 통해<br>
+                기업의 사회적 책임 이행을 유도하고 상생과 협력을 촉진합니다.
+              </div>
+            </div>
+          </li>
+          <div class="slide-number con-1 ">
+            <div class="btn-box row">
+              <div class="no-1 no- cell active"><span>01</span></div>
+              <div class="no-2 no- cell"><span>02</span></div>
+              <div class="no-3 no- cell"><span>03</span></div>
+              <div class="no-4 no- cell"><span>04</span></div>
+              <div class="no-5 no- cell"><span>05</span></div>
+            </div>
+          </div>
         </ul>
       </div>
+      <div class="right-side-menu-box row">
+        <ul class="top-menu-box">
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+        </ul>
+        <ul class="bottom-menu-box row">
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+          <li class="cell"><a href="#" class="block"></a></li>
+        </ul>
+      </div>
+      <div class="main-page-side-menu con-1 relative">
+        <nav class="side-menu-box color-fff text-align-center">
+          <ul class="top-menu row">
+            <li class="login cell"><a href="#" class="block"><span class="icon"></span><span class="text">로그인</span></a></li>
+            <li class="certification-center cell"><a href="#" class="block"><span class="text">인증센터</span></a></li>
+          </ul>
+          <ul class="bottom-menu row">
+            <li class="account-inquiry cell"><a href="#" class="block"><span class="icon"></span><span class="text">계좌조회</span></a><div class="hover-bg"></div></li>
+            <li class="transfer cell"><a href="#" class="block"><span class="icon"></span><span class="text">이체</span></a><div class="hover-bg"></div></li>
+            <li class="new-deposit cell"><a href="#" class="block"><span class="icon"></span><span class="text">예금신규</span></a><div class="hover-bg"></div></li>
+            <li class="card-application cell"><a href="#" class="block"><span class="icon"></span><span class="text">카드신청</span></a><div class="hover-bg"></div></li>
+            <li class="quick-inquiry cell"><a href="#" class="block"><span class="icon"></span><span class="text">빠른조회</span></a><div class="hover-bg"></div></li>
+            <li class="consumer-protection cell"><a href="#" class="block"><span class="icon"></span><span class="text">금융소비자보호</span></a><div class="hover-bg"></div></li>
+          </ul>
+        </nav>
+        <div class="side-banner con-1">
+          <div class="img"><a href="#" class="block"></a></div>
+        </div>
+      </div>
+      <div class="scroll-guide color-fff absolute-bottom">Scroll</div>
+      <div class="main-page-bottom-menu absolute-bottom color-fff">
+        <nav class="bottom-menu-box con-1 row">
+        <div class="news cell"><a href="#" class="block">새소식<i class="fas fa-chevron-right"></i></a></div>
+          <ul class="left-menu cell">
+            <li class="row"><a href="#" class="block cell">뱅크월렛 서비스 종료 안내</a><span class="date cell">2020-08-24</span></li>
+            <li class="row"><a href="#" class="block cell">오픈뱅킹 서비스 일시 중단 안내(8/22(토))</a><span class="date cell">2020-08-19</span></li>
+          </ul>
+          <div class="announcement cell"><a href="#" class="block">입찰공고<i class="fas fa-chevron-right"></i></a></div>
+          <ul class="right-menu cell">
+            <li class="row"><a href="#" class="block cell">전산자료 통합반출 시스템 구축</a><span class="date cell">2020-08-27</span></li>
+            <li class="row"><a href="#" class="block cell">RPA 본사업 컨설팅 제안평가서 결과 공개</a><span class="date cell">2020-08-26</span></li>
+          </ul>
+        </nav>
+      </div>
     </div>
+    <!-- section .2 HTML -->
+    <div class="section">
+
+    </div>
+    <!-- section .3 HTML -->
     <div class="section">
 
     </div>
--- js/common.js
+++ js/common.js
@@ -1,21 +1,21 @@
 /* fullpage jQuery */
 function full() {
   new fullpage('#fullpage', {
-    sectionsColor: ['', '', '', '' ,''],
+    sectionsColor: ['', '#E8EDF3', '#ECEAE7', '', ''],
     verticalCentered: false,
     scrollBar: false,
-    anchors: ['anchor1', 'anchor2', 'anchor3', 'anchor4' ,'anchor5'],
+    anchors: ['anchor1', 'anchor2', 'anchor3', 'anchor4', 'anchor5'],
     menu: '#menu',
   });
 }
 
 /* hover-menu-deco */
-function hover_menu_underline(){
+function hover_menu_underline() {
   var $under_bar = $('.hover-item');
-  $('.top-menu-bar>.menu-box-2-background>.menu-box-2>.text-menu>.text-item').mouseenter(function(){
+  $('.top-menu-bar>.menu-box-2-background>.menu-box-2>.text-menu>.text-item').mouseenter(function () {
     var orginWidth = $(this).width();
     var originLeft = $(this).position().left;
-    
+
 
     $under_bar.addClass('active');
     $under_bar.css({
@@ -25,7 +25,7 @@ function hover_menu_underline(){
 
     console.log(orginWidth);
   })
-  $('.top-menu-bar>.menu-box-2-background>.menu-box-2>.text-menu>.text-item').mouseleave(function(){
+  $('.top-menu-bar>.menu-box-2-background>.menu-box-2>.text-menu>.text-item').mouseleave(function () {
     $under_bar.removeClass('active');
   })
 }
@@ -49,8 +49,24 @@ function hover_menu_underline(){
 }
 */
 
+function main_slide() {
+
+  $('.main-slide-banner>.image-slider>.slide-number>.btn-box>.no-').click(function () {
+    var index = $(this).index();
+    var $imgActive = $('.section>.main-slide-banner>.image-slider').find('> .active');
+    var $numberActive = $('.section>.main-slide-banner>.image-slider>.slide-number>.btn-box').find('> .active');
+
+    $numberActive.removeClass('active');
+    $(this).addClass('active');
+    $imgActive.removeClass('active');
+    $('.main-slide-banner>.image-slider>li').eq(index).addClass('active');
+  });
+}
+
+
 
 $(function () {
   full();
   hover_menu_underline();
+  main_slide();
 });
